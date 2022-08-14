1dcc92128c4daff667bc37163544657fe69b0355
BulavkinaValeria <lerabulavkina.a@gmail.com>
2022-08-14 15:21:27 +0300

    Init4

--- src/Homework2.java
+++ src/Homework2.java
@@ -1,7 +1,8 @@
 public class Homework2 {
     public static void main(String[] args) {
         int a = 1;
-
+        int b = 2;
         System.out.println(a + " ");
+        System.out.println(b);
     }
 }
