738a793ba9f604dd0c76358bd11ffdc921094bba
BulavkinaValeria <lerabulavkina.a@gmail.com>
2022-08-14 15:23:33 +0300

    Init7

--- src/Homework.java
+++ src/Homework.java
@@ -2,6 +2,7 @@ public class Homework {
     public static void main(String[] args) {
         int a = 1;
         int d = 6;
+        System.out.println();
         System.out.println(a);
         System.out.println(d);
         System.out.println();
